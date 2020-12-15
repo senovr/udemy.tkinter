50071a7d4e03c9516a80d95c5cf94451835d3920
Sergey Parkhonyuk <serp2006@gmail.com>
2020-12-15 23:22:58 +0700

    update basic window script as per video lecture

--- pre_lesson_0/basics_window.py
+++ pre_lesson_0/basics_window.py
@@ -2,4 +2,14 @@ import tkinter
 root=tkinter.Tk()
 root.title('Window basics!')
 root.iconbitmap('thinking.ico')
+root.geometry('250x700')
+root.resizable(0,0)
+root.config(bg=  '#999999')
+
+#making second window
+top=tkinter.Toplevel()
+top.title('Second window')
+top.config(bg='#003366')
+top.geometry('200x200+500+50')
+# run root's window main loop (forever cycle, until close (X) is clicked)
 root.mainloop()
